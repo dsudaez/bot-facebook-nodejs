3a4c4bc894ff6da3d5abd8ef74850e95b2bf526c
Deb Sudañez <deb.sudaez@gmail.com>
2016-09-29 04:01:12 -0300

    Agregando toke_verify

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
-
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'Que_Pasa_Contig0') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
